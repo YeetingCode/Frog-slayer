1963a0a5487fdd0ad75a0413034b0f67af745edc
YeetingCode <113666377+YeetingCode@users.noreply.github.com>
2022-12-23 19:17:23 +0100

    Last Push Xandar

--- Enemy.gd
+++ Enemy.gd
@@ -6,14 +6,21 @@ const gravity = 40
 
 var motion = Vector2()
 
-func _ready() -> void:
+func _ready():
 	motion.x = -speed
 
-func _physics_process(delta: float) -> void:
+func _on_StompDetector_body_entered(body: KinematicBody2D):
+	if body.global_position.y < get_node("StompDetector").global_position.y:
+		queue_free()
+
+
+
+
+func _physics_process(delta):
+	
+
 	motion.y += gravity
 	
 	if is_on_wall():
 		motion.x *= -1.0
 	motion.y = move_and_slide(motion, UP).y
-	
-	
